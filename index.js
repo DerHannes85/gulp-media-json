0b8702d0173f950929cb635897b27f53c5067350
Hannes <hannes.weil@gmail.com>
2018-10-23 00:32:28 +0200

    Add basic audio/video support

--- index.js
+++ index.js
@@ -14,36 +14,41 @@ const PluginError = require('plugin-error');
 const Through = require('through2');
 const Vinyl = require('vinyl');
 
+const mimeType = require('mime-type/with-db')
 const Jimp = require('jimp');
 
 const PLUGIN_NAME = 'gulp-media-json';
 
-const gcd = function(a, b) {
-    if (!b) {
+const gcd = function (a, b) {
+    if (!b)
+    {
         return a;
     }
 
     return gcd(b, a % b);
 };
 
-const namespace = function(obj, path, value) {
+const namespace = function (obj, path, value) {
     let tempValue;
 
-    if (typeof path === 'undefined') {
+    if (typeof path === 'undefined')
+    {
         path = obj;
         obj = this;
     }
 
     tempValue = _get(obj, path, undefined);
-    if (typeof tempValue === 'undefined') {
+    if (typeof tempValue === 'undefined')
+    {
         _set(obj, path, value);
-    } else {
+    } else
+    {
         value = tempValue;
     }
     return value;
 };
 
-const escapeNamespace = function(s) {
+const escapeNamespace = function (s, ext) {
     let temp,
         i;
 
@@ -51,11 +56,14 @@ const escapeNamespace = function(s) {
 
     temp = s.split(/\./);
     s = "";
-    for (i = 0; i < temp.length; i++) {
-        if (i !== 0) {
+    for (i = 0; i < temp.length; i++)
+    {
+        if (i !== 0)
+        {
             s += '.';
         }
-        if (temp[i][0].match(/[0-9]/) !== null) {
+        if (temp[i][0].match(/[0-9]/) !== null)
+        {
             s += '_';
         }
         s += _camelCase(temp[i]);
@@ -64,13 +72,14 @@ const escapeNamespace = function(s) {
     return s;
 };
 
-module.exports = function(settings) {
+module.exports = function (settings) {
     let options = {
             // Defaults
             escapeNamespace: escapeNamespace,
             fileName: 'media.json',
-            emptyBase64: true,
-            emptyBase64Namespace: null,
+            emptyImageBase64: true,
+            emptyImageBase64Namespace: null,
+            ratioValue: false,
             startObj: {},
             endObj: null,
             exportModule: false,
@@ -83,30 +92,39 @@ module.exports = function(settings) {
         base64Data;
 
 
-    if (typeof settings === 'object') {
+    if (typeof settings === 'object')
+    {
         options = Object.assign(options, settings);
     }
 
-    if (typeof options.startObj === 'object') {
+    if (typeof options.startObj === 'object')
+    {
         returnData = options.startObj;
-    } else {
+    } else
+    {
         returnData = {};
     }
 
-    if (options.emptyBase64) {
+    if (options.emptyImageBase64)
+    {
         base64Data = {};
     }
 
-    function processFile(file, enc, cb) {
+    function processFile(file, enc, cb)
+    {
         let dataNamespace,
-            currentVinyl;
+            currentVinyl,
+            currentMimeType,
+            currentExtName;
 
-        if (file.isNull()) {
+        if (file.isNull())
+        {
             cb();
             return;
         }
 
-        if (file.isStream()) {
+        if (file.isStream())
+        {
             this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported!'));
             cb();
             return;
@@ -114,77 +132,121 @@ module.exports = function(settings) {
 
         lastFile = file;
         currentVinyl = new Vinyl(file);
+        currentMimeType = mimeType.lookup(currentVinyl.basename);
+        currentExtName = currentVinyl.extname.replace(/^\./, '');
 
         dataNamespace = currentVinyl.dirname.replace(currentVinyl.cwd + '\\' + options.basePath, '').replace(/[\\\/]/g, '.').replace(/^\./g, '');
-        dataNamespace = options.escapeNamespace(dataNamespace + '.' + currentVinyl.stem);
+        dataNamespace = options.escapeNamespace(dataNamespace + '.' + currentVinyl.stem, currentExtName);
 
         namespace(returnData, dataNamespace, {
-            src: currentVinyl.dirname.replace(options.basePath, '').replace(/[\\]/g, '/') + '/' + currentVinyl.basename
+            src: currentVinyl.dirname.replace(currentVinyl.cwd + '\\' + options.basePath, '').replace(/[\\]/g, '/') + '/' + currentVinyl.basename,
+            ext: currentExtName,
+            mime: currentMimeType
         });
 
-        Jimp.read(file.path)
-            .then((image) => {
-                let currentGcd = gcd(image.bitmap.width, image.bitmap.height),
-                    ratioName = (image.bitmap.width / currentGcd) + '/' + (image.bitmap.height / currentGcd),
-                    base64DataElement = _get(base64Data, ratioName, false);
+        switch (true)
+        {
+            case (currentMimeType.match(/image/) !== null):
+                _set(returnData, dataNamespace + '.type', 'image');
 
-                _set(returnData, dataNamespace + '.width', image.bitmap.width);
-                _set(returnData, dataNamespace + '.height', image.bitmap.height);
-                _set(returnData, dataNamespace + '.ratioName', ratioName);
-                _set(returnData, dataNamespace + '.ratioValue', image.bitmap.width / image.bitmap.height);
+                Jimp.read(file.path)
+                    .then((image) => {
+                        let currentGcd = gcd(image.bitmap.width, image.bitmap.height),
+                            ratioName = (image.bitmap.width / currentGcd) + '/' + (image.bitmap.height / currentGcd),
+                            base64DataElement = _get(base64Data, ratioName, false);
 
-                if (options.emptyBase64) {
-                    if (base64DataElement !== false) {
-                        if (typeof options.emptyBase64Namespace !== 'string') {
-                            _set(returnData, dataNamespace + '.empty', base64DataElement);
+                        _set(returnData, dataNamespace + '.w', image.bitmap.width);
+                        _set(returnData, dataNamespace + '.h', image.bitmap.height);
+                        _set(returnData, dataNamespace + '.ratio', ratioName);
+                        if (options.ratioValue === true)
+                        {
+                            _set(returnData, dataNamespace + '.ratioValue', image.bitmap.width / image.bitmap.height);
                         }
-                        cb();
-                    } else {
-                        new Jimp(image.bitmap.width / currentGcd, image.bitmap.height / currentGcd, (err, emptyImage) => {
-                            if (err === null) {
-                                emptyImage.getBase64Async(Jimp.MIME_PNG)
-                                    .then((newBase64DataElement) => {
-                                        // cache base64
-                                        _set(base64Data, ratioName, newBase64DataElement);
-                                        if (typeof options.emptyBase64Namespace !== 'string') {
-                                            _set(returnData, dataNamespace + '.empty', newBase64DataElement);
-                                        }
-                                        cb();
-                                    });
-                            } else {
+
+                        if (options.emptyImageBase64)
+                        {
+                            if (base64DataElement !== false)
+                            {
+                                if (typeof options.emptyImageBase64Namespace !== 'string')
+                                {
+                                    _set(returnData, dataNamespace + '.empty', base64DataElement);
+                                }
                                 cb();
+                            } else
+                            {
+                                new Jimp(image.bitmap.width / currentGcd, image.bitmap.height / currentGcd, (err, emptyImage) => {
+                                    if (err === null)
+                                    {
+                                        emptyImage.getBase64Async(Jimp.MIME_PNG)
+                                            .then((newBase64DataElement) => {
+                                                // cache base64
+                                                _set(base64Data, ratioName, newBase64DataElement);
+                                                if (typeof options.emptyImageBase64Namespace !== 'string')
+                                                {
+                                                    _set(returnData, dataNamespace + '.empty', newBase64DataElement);
+                                                }
+                                                cb();
+                                            });
+                                    } else
+                                    {
+                                        cb();
+                                    }
+                                });
                             }
-                        });
-                    }
-                } else {
-                    cb();
-                }
-            })
-            .catch((err) => {
-                this.emit('error', new PluginError(PLUGIN_NAME, 'Error while processing image ' + file.pathname));
+                        } else
+                        {
+                            cb();
+                        }
+                    })
+                    .catch(() => {
+                        this.emit('error', new PluginError(PLUGIN_NAME, 'Error while processing image ' + file.pathname));
+                        cb();
+                    });
+
+                break;
+
+            case (currentMimeType.match(/video/) !== null):
+                _set(returnData, dataNamespace + '.type', 'video');
                 cb();
-            });
+                break;
+
+            case (currentMimeType.match(/audio/) !== null):
+                _set(returnData, dataNamespace + '.type', 'audio');
+                cb();
+                break;
+
+            default:
+                _set(returnData, dataNamespace + '.type', 'unknown');
+                cb();
+                break;
+        }
     }
 
-    function endStream(cb) {
-        if (!lastFile) {
+    function endStream(cb)
+    {
+        if (!lastFile)
+        {
             cb();
             return;
         }
 
-        if (options.emptyBase64 && typeof options.emptyBase64Namespace === 'string') {
-            namespace(returnData, options.emptyBase64Namespace, base64Data);
+        if (options.emptyImageBase64 && typeof options.emptyImageBase64Namespace === 'string')
+        {
+            namespace(returnData, options.emptyImageBase64Namespace, base64Data);
         }
 
-        if (options.endObj) {
+        if (options.endObj)
+        {
             returnData = _.assign(returnData, options.endObj);
         }
 
         let contents = JSON.stringify(returnData, options.jsonReplacer, options.jsonSpace);
 
-        if (options.exportModule === true) {
+        if (options.exportModule === true)
+        {
             contents = `module.exports = ${contents};`;
-        } else if (options.exportModule) {
+        } else if (options.exportModule)
+        {
             contents = `${options.exportModule} = ${contents};`;
         }
 
